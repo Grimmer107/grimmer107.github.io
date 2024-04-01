3eac89aee29748066902387986e003c9fa606f53
Grimmer107 <u26.qureshi@gmail.com>
2024-04-01 07:34:55 +0500

    feat: add experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import Tabs from "@mui/material/Tabs"
 import Tab from "@mui/material/Tab"
 import Typography from "@mui/material/Typography"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
 import Box from "@mui/material/Box"
 
 interface TabPanelProps {
@@ -9,6 +10,12 @@ interface TabPanelProps {
 	index: number
 	value: number
 }
+
+const theme = createTheme({
+	palette: {
+		primary: { main: "#2bbc8a" }
+	}
+})
 
 function TabPanel(props: TabPanelProps) {
 	const { children, value, index, ...other } = props
@@ -19,11 +26,13 @@ function TabPanel(props: TabPanelProps) {
 			hidden={value !== index}
 			id={`vertical-tabpanel-${index}`}
 			aria-labelledby={`vertical-tab-${index}`}
+			className="font-ubuntu color-[]"
 			{...other}
 		>
 			{value === index && (
 				<Box sx={{ p: 3 }}>
-					<Typography>{children}</Typography>
+					{/* <Typography>{children}</Typography> */}
+					{children}
 				</Box>
 			)}
 		</div>
@@ -45,56 +54,109 @@ export default function Experience() {
 	}
 
 	return (
-		<div className="m-auto mt-[2em] text-center">
+		<div className="m-auto my-[2em] text-center bg-[#202023] flex flex-col justify-center align-middle">
 			<p className="font-ubuntu text-[#2bbc8a] m-auto mt-[1em] text-[1.25rem]">
 				Experience
 			</p>
-			<Box
-				sx={{
-					flexGrow: 1,
-					bgcolor: "background.paper",
-					display: "flex",
-					height: 224
-				}}
-			>
-				<Tabs
-					orientation="vertical"
-					variant="scrollable"
-					value={value}
-					onChange={handleChange}
-					aria-label="Vertical tabs example"
-					sx={{ borderRight: 1, borderColor: "divider" }}
-				>
-					<Tab label="Item One" {...a11yProps(0)} />
-					<Tab label="Item Two" {...a11yProps(1)} />
-					<Tab label="Item Three" {...a11yProps(2)} />
-					<Tab label="Item Four" {...a11yProps(3)} />
-					<Tab label="Item Five" {...a11yProps(4)} />
-					<Tab label="Item Six" {...a11yProps(5)} />
-					<Tab label="Item Seven" {...a11yProps(6)} />
-				</Tabs>
-				<TabPanel value={value} index={0}>
-					Item One
-				</TabPanel>
-				<TabPanel value={value} index={1}>
-					Item Two
-				</TabPanel>
-				<TabPanel value={value} index={2}>
-					Item Three
-				</TabPanel>
-				<TabPanel value={value} index={3}>
-					Item Four
-				</TabPanel>
-				<TabPanel value={value} index={4}>
-					Item Five
-				</TabPanel>
-				<TabPanel value={value} index={5}>
-					Item Six
-				</TabPanel>
-				<TabPanel value={value} index={6}>
-					Item Seven
-				</TabPanel>
-			</Box>
+			<div className="flex flex-col justify-center align-middle min-w-[35em] m-auto mt-[1em]">
+				<ThemeProvider theme={theme}>
+					<Box
+						sx={{
+							flexGrow: 1,
+							bgcolor: "#202023",
+							display: "flex",
+							minHeight: "24em",
+							color: "white"
+						}}
+					>
+						<Tabs
+							orientation="vertical"
+							value={value}
+							onChange={handleChange}
+							aria-label="Work Experiences"
+							sx={{
+								borderRight: 1,
+								borderColor: "#2bbc8a"
+							}}
+							TabIndicatorProps={{
+								style: {
+									backgroundColor: "#2bbc8a",
+									color: "#2bbc8a"
+								}
+							}}
+						>
+							<Tab
+								label="ThinkSoft"
+								{...a11yProps(0)}
+								sx={{
+									color: "white",
+									fontFamily: "Ubuntu Mono"
+								}}
+							/>
+							<Tab
+								label="Arbisoft"
+								{...a11yProps(1)}
+								sx={{
+									color: "white",
+									fontFamily: "Ubuntu Mono"
+								}}
+							/>
+						</Tabs>
+						<TabPanel value={value} index={0}>
+							<p className="w-[30em] break-words text-wrap text-left">
+								Worked as Full-Stack Intern on a cutting-edge
+								web development tool called WebbsAI that
+								combines drag-and-drop functionality with
+								Generative AI assistance to simplify website
+								creation process.
+							</p>
+							<br />
+							<div className="w-[30em] break-words text-wrap text-left">
+								<li>
+									<span>
+										Implemented secure and efficient API
+										endpoints using the
+										&apos;next-connect&apos; package.
+									</span>
+								</li>
+								<li>
+									<span>
+										Added a comprehensive SEO and OpenGraph
+										settings feature with SERP preview for
+										websites created using WebbsAI.
+									</span>
+								</li>
+								<li>
+									<span>
+										Introduced a feature for wrapping
+										selected text with typography components
+										in web editor.
+									</span>
+								</li>
+								<li>
+									<span>
+										Leveraged TypeScript to ensure type
+										safety and produce robust code,
+										enhancing reliability and
+										maintainability.
+									</span>
+								</li>
+								<li>
+									<span>
+										Developed WebbsAI product website using
+										Next.js, Tailwind CSS, and TypeScript.
+									</span>
+								</li>
+							</div>
+						</TabPanel>
+						<TabPanel value={value} index={1}>
+							<p className="w-[30em] text-left">
+								Starting in June, 2024
+							</p>
+						</TabPanel>
+					</Box>
+				</ThemeProvider>
+			</div>
 		</div>
 	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar"
 import Intro from "@/components/intro"
+import Experience from "@/components/experience"
 
 export default function Home() {
 	return (
 		<div>
 			<Navbar />
 			<Intro />
+			<Experience />
 		</div>
 	)
 }
