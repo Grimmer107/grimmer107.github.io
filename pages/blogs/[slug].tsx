08b78a42e3b31d21e20e8267f130e2839bcb52f0
Grimmer107 <u26.qureshi@gmail.com>
2024-04-05 15:21:19 +0500

    fix build error

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -21,8 +21,8 @@ const Month = {
 } as any
 
 const client = createClient({
-	space: process.env.CONTENTFUL_SPACE_ID as string,
-	accessToken: process.env.CONTENTFUL_ACCESS_KEY as string
+	space: "40owv3zaeflf",
+	accessToken: "BNVYh2iX4gOYpByH8zqNItwaXzGNZCGIc--4lRUkwWw"
 })
 
 export const getStaticPaths = async () => {
--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -5,8 +5,8 @@ import { createClient } from "contentful"
 
 export async function getStaticProps() {
 	const client = createClient({
-		space: process.env.CONTENTFUL_SPACE_ID as string,
-		accessToken: process.env.CONTENTFUL_ACCESS_KEY as string
+		space: "40owv3zaeflf",
+		accessToken: "BNVYh2iX4gOYpByH8zqNItwaXzGNZCGIc--4lRUkwWw"
 	})
 
 	const res = await client.getEntries({ content_type: "tech-blog" })
