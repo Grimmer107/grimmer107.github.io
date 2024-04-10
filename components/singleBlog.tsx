1e88ec11d3d6cdbfdff69bb336c075e67d6517de
Grimmer107 <u26.qureshi@gmail.com>
2024-04-10 23:54:18 +0500

    feat: add media queries for blog page

--- components/singleBlog.tsx
+++ components/singleBlog.tsx
@@ -35,60 +35,58 @@ export default function SingleBlog({ blog }: any) {
 	} = blog.fields
 
 	return (
-		<div className="m-auto my-[1em] bg-[#18181B] shadow-md shadow-black hover:shadow-lg hover:shadow-black transition-shadow duration-500 ease-out cursor-pointer border-[1px] rounded-[0.5rem] border-[#27272A] py-[1em] px-[1.5em] w-[70%] h-[15em] flex flex-row justify-center align-middle">
-			<div className="relative h-[12em] w-[40em] m-auto flex flex-col justify-center align-middle rounded-[0.5em] mr-[1em] overflow-clip">
+		<div className="m-auto my-[1em] flex min-h-[15em] w-[90%] flex-col justify-center rounded-[0.5rem] border-[1px] border-[#27272A] bg-[#18181B] px-[1.5em] py-[1.5em] align-middle shadow-md shadow-black transition-shadow duration-500 ease-out hover:shadow-lg hover:shadow-black min-[390px]:w-[70%] md:flex md:flex-row  md:py-[1em]">
+			<div className="relative m-auto mr-[1em] flex h-[12em] w-[100%] flex-col justify-center overflow-clip rounded-[0.5em] align-middle md:w-[40%]">
 				<Image
 					src={`https:${thumbnail.fields.file.url}`}
 					alt={thumbnail.fields.title}
 					layout="fill"
 					style={{ objectFit: "cover" }}
-					className="absolute inset-0 rounded-[0.5em] transition-transform duration-500 transform hover:scale-125 ease-out"
+					className="absolute inset-0 h-[100%] transform rounded-[0.5em] transition-transform duration-500 ease-out hover:scale-125"
 				/>
 			</div>
-			<div className="font-oxygen font-semibold py-[1em]">
-				<div className="m-auto">
-					<p className="break-words text-wrap text-[#F97316] hover:underline underline-offset-2">
-						<Link href={`blogs/${slug}`}>{title}</Link>
+			<div className="w-[100%] pt-[1em] font-oxygen font-semibold md:w-[60%]">
+				<p className="text-wrap break-words text-[0.8rem] text-[#F97316] underline-offset-2 hover:underline sm:text-[0.85rem] md:text-[1rem]">
+					<Link href={`blogs/${slug}`}>{title}</Link>
+				</p>
+				<div
+					className="mb-[0.25em] h-[6em] w-[100%] overflow-clip"
+					// style={{
+					// 	whiteSpace: "nowrap",
+					// 	overflow: "hidden",
+					// 	textOverflow: "ellipsis"
+					// }}
+				>
+					<p className="text-wrap break-words text-[0.75rem] font-normal text-[#A1A1AA] sm:text-[0.8rem] md:text-[0.85rem]">
+						{featuredText}
 					</p>
-					<div
-						className="h-[6em] w-[30em]"
-						style={{
-							whiteSpace: "nowrap",
-							overflow: "hidden",
-							textOverflow: "ellipsis"
-						}}
+				</div>
+				<div className="mt-[1em] flex justify-items-start align-middle text-[0.6rem] font-normal text-white min-[390px]:text-[0.7rem] md:text-[0.8rem]">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="my-auto inline-block h-6 w-[1.375rem] scale-75 fill-white"
+						aria-hidden="true"
 					>
-						<p className="text-[#A1A1AA] text-[0.85rem] font-normal text-wrap">
-							{featuredText}
-						</p>
-					</div>
-					<div className="flex flex-row text-white text-[0.8rem] justify-items-start align-middle mt-[1em] font-normal">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="inline-block fill-white h-6 scale-75 w-[1.375rem] my-auto"
-							aria-hidden="true"
-						>
-							<path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
-							<path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
-						</svg>
-						<p className="my-auto">
-							&nbsp;{getFormatedTime(publishedTime)}&nbsp;&nbsp;
-							<span className="scale-110">|</span>&nbsp;&nbsp;
-						</p>
-						<svg
-							stroke="white"
-							fill="white"
-							strokeWidth="0"
-							viewBox="0 0 24 24"
-							height="1.25em"
-							width="1.25em"
-							xmlns="http://www.w3.org/2000/svg"
-							className="scale-110 my-auto"
-						>
-							<path d="M12.25 2c-5.514 0-10 4.486-10 10s4.486 10 10 10 10-4.486 10-10-4.486-10-10-10zM18 13h-6.75V6h2v5H18v2z"></path>
-						</svg>
-						<p className="my-auto ml-[12px]">{readingTime} min</p>
-					</div>
+						<path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
+						<path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
+					</svg>
+					<p className="my-auto">
+						&nbsp;{getFormatedTime(publishedTime)}
+						<span className="mx-[1em] scale-110">|</span>
+					</p>
+					<svg
+						stroke="white"
+						fill="white"
+						strokeWidth="0"
+						viewBox="0 0 24 24"
+						height="1.25em"
+						width="1.25em"
+						xmlns="http://www.w3.org/2000/svg"
+						className="my-auto scale-110"
+					>
+						<path d="M12.25 2c-5.514 0-10 4.486-10 10s4.486 10 10 10 10-4.486 10-10-4.486-10-10-10zM18 13h-6.75V6h2v5H18v2z"></path>
+					</svg>
+					<p className="my-auto ml-[12px]">{readingTime} min</p>
 				</div>
 			</div>
 		</div>
--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -37,8 +37,8 @@ export default function Blog({ blogs }: any) {
 			<Head>
 				<title>Usama Qureshi | Blogs</title>
 			</Head>
-			<section className="flex flex-col justify-center align-middle m-auto w-[100%] h-[24em] pt-[4em]">
-				<div className="m-auto text-center w-[100%] h-[20em] relative">
+			<section className="m-auto flex h-[24em] w-[100%] flex-col justify-center pt-[4em] align-middle">
+				<div className="relative m-auto h-[20em] w-[100%] text-center">
 					<video
 						width="100%"
 						height="100%"
@@ -46,17 +46,17 @@ export default function Blog({ blogs }: any) {
 						loop
 						muted
 						preload="none"
-						className="inset-0 opacity-30 object-cover absolute h-[20em]"
+						className="absolute inset-0 h-[20em] object-cover opacity-30"
 					>
 						<source src="/masthead.webm" type="video/mp4" />
 						Your browser does not support the video tag.
 					</video>
 				</div>
-				<div className="m-auto text-center w-[100%] text-[#F97316] absolute opacity-90">
-					<p className="text-[2.25rem] font-jetbrain font-medium">
+				<div className="absolute m-auto w-[100%] text-center text-[#F97316] opacity-90">
+					<p className="font-jetbrain text-[2.25rem] font-medium">
 						Blog
 					</p>
-					<p className="text-white text-[1rem] font-ubuntu m-auto text-center mt-[1em] w-[60%]">
+					<p className="m-auto mt-[1em] w-[90%] text-center font-ubuntu text-[0.85rem] text-white md:w-[60%] md:text-[1rem]">
 						Welcome to my blog domain where I share personal stories
 						about things I&apos;ve learned, projects I&apos;m
 						hacking on and just general findings. I also write for
@@ -64,8 +64,8 @@ export default function Blog({ blogs }: any) {
 					</p>
 				</div>
 			</section>
-			<section className="flex flex-col justify-center align-middle m-auto w-[100%] py-[3em]">
-				<div className="m-auto flex flex-row text-left w-[70%] mb-[1em]">
+			<section className="m-auto flex w-[100%] flex-col justify-center py-[3em] align-middle">
+				<div className="m-auto mb-[1em] flex w-[70%] flex-row text-left">
 					<svg
 						stroke="white"
 						fill="white"
@@ -79,7 +79,7 @@ export default function Blog({ blogs }: any) {
 						<path d="M20 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zM4 19V5h16l.002 14H4z"></path>
 						<path d="M6 7h12v2H6zm0 4h12v2H6zm0 4h6v2H6z"></path>
 					</svg>
-					<p className="text-[1.25rem] font-ubuntu text-white ml-[0.5em]">
+					<p className="ml-[0.5em] font-ubuntu text-[1.25rem] text-white">
 						Explore All
 					</p>
 				</div>
